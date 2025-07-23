67dc9728aa95a1a4d89176e1ee61bb3d8746ef0b
Sai Prabhu <gsaiprabhu@gmail.com>
2025-07-24 00:13:03 +0530

    Added Base code.

---
+++ src/main/java/com/myproject/thelensoryco/TheLensoryCoApplication.java
@@ -0,0 +1,18 @@
+package com.myproject.thelensoryco;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class TheLensoryCoApplication
+{
+    public static void main( MysqlxDatatypes.Scalar.String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
